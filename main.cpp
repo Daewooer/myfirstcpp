48796d5dc8a451c536108681a0cbd0e4a19f4ce7
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-05-06 13:08:41 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+string sexable = "sex bomb";
+int number1 = 1;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello World!";
+  return 0;
+}
+
+int x {
+  cout << "What is your number??"
+  cin >> x;
+  cout << "Your number is " << x;
+  return 0;
+}
+
+int tht {
+  cout << "What is your username?";
+  cin >> sexable;
+  cout << "Your username is " << tht;
+  return 0;
+}
+
+string cars [4] = {"Volvo", "BMW", "Ford", "Mazda"};
+
+string &meal = food;
+
+cout << meal
+
+int x = 100 + 50;
+
+
+int sum1 = 100 + 50; //=150
